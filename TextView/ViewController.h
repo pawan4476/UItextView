468e81f8c1c63f016c2b274094afe44d35bd8d3f
pawan4476 <nagam.tiger@gmail.com>
2016-09-26 15:06:47 +0530

    Initial commit

---
+++ TextView/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  TextView
+//
+//  Created by Nagam Pawan on 9/14/16.
+//  Copyright © 2016 Nagam Pawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    NSString *str;
+}
+@property (strong, nonatomic) IBOutlet UITextView *txt;
+
+
+@end
+
---
+++ TextView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextView
+//
+//  Created by Nagam Pawan on 9/14/16.
+//  Copyright © 2016 Nagam Pawan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    str = @"This is what i have said aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    _txt.text = str;
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
